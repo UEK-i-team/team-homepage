ec5c0a0baf4043ba5d6ce78eae32281b7334428a
Szymon Brud <szymon.brud@crodu.com>
2023-12-20 18:39:51 +0100

    fix: test

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,8 @@
+import './';
+
 import * as React from 'react';
 
 const NotFoundPage = () => {
-  const a = {};
-
-  a.eb().db();
   return <h1 style={{ fontFamily: 'sans-serif' }}>404 - page not found</h1>;
 };
 
