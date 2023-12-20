79f08009aacf849f0302e176e43dbab4a26dee31
Szymon Brud <szymon.brud@crodu.com>
2023-12-20 18:26:08 +0100

    fix: test

--- src/components/TitleText/TitleText.js
+++ src/components/TitleText/TitleText.js
@@ -3,5 +3,5 @@ import React from 'react';
 import { title } from './TitleText.module.scss';
 
 export const TitleText = () => (
-  <h1 className={`${title} bg-dark`}>Hello on team-homepage</h1>
+  <h1 className={`${title} bg-dark`}>Hello on team-homepage test</h1>
 );
