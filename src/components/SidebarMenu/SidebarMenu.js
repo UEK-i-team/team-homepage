d2232cbb8a1076cbb928a6e0d18cec4a7e614d65
TheWoxPL <wojtek.bubula@onet.pl>
2023-12-21 00:03:42 +0100

    feature: add new styles

--- src/components/SidebarMenu/SidebarMenu.js
+++ src/components/SidebarMenu/SidebarMenu.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { container, menu } from './SidebarMenu.module.scss';
+import CaronSVG from './caron.svg';
+import {
+  caronItem,
+  caronItemRotated,
+  container,
+  menu,
+  menuItem,
+  menuItemDecoration,
+  menuItemProjects,
+  projectsOption,
+  projectsOptionHidden,
+} from './SidebarMenu.module.scss';
 
 export const SidebarMenu = () => {
+  const [projects, useProjects] = useState(false);
+  // const [projectsOption, useProjects] = useState(false)
+
+  const handleToggleProjects = () => {
+    useProjects(!projects);
+  };
+
   return (
     <div className={container}>
-      {/* <div className={close}></div> */}
-      <div>
-        <ul className={menu}>
-          <li>HOME</li>
-          <li>
-            PROJEKTY
-            <ul>
-              <li>MAPA UCZELNI</li>
-              <li>TAJNY PROJEKT PROJEKT 339</li>
-            </ul>
-          </li>
-          <li>AKTUALNOŚCI</li>
-          <li>DOŁĄCZ DO NAS</li>
-          <li>KONTAKT</li>
-        </ul>
+      <div className={menu}>
+        <div className={menuItem}>HOME</div>
+        <div className={projects ? menuItemProjects : menuItem}>
+          <img
+            src={CaronSVG}
+            className={projects ? caronItem : caronItemRotated}
+            onClick={handleToggleProjects}
+          />
+          <span>PROJEKTY</span>
+        </div>
+        <div>
+          <div className={projects ? projectsOption : projectsOptionHidden}>
+            <div className={menuItemDecoration}></div>
+            <span>MAPA UCZELNI</span>
+          </div>
+          <div className={projects ? projectsOption : projectsOptionHidden}>
+            <div className={menuItemDecoration}></div>
+            <span>TAJNY PROJEKT PROJEKT 339</span>
+          </div>
+        </div>
+        <div className={menuItem}>AKTUALNOŚCI</div>
+        <div className={menuItem}>DOŁĄCZ DO NAS</div>
+        <div className={menuItem}>KONTAKT</div>
       </div>
     </div>
   );
