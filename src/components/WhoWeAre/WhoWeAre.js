9fc9a76d74c827509160bb201936210892ae407c
Dawid <david.s17q@gmail.com>
2024-01-03 23:17:30 +0100

    feature: added WhoWeAre component

--- src/components/WhoWeAre/WhoWeAre.js
+++ src/components/WhoWeAre/WhoWeAre.js
@@ -1,23 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import { ThemeContext } from '../../context/ThemeContext';
-import {
-  background,
-  backgroundDark,
-  container,
-  h1,
-  h1Dark,
-  p,
-  pDark,
-} from '../WhoWeAre/WhoWeAre.module.scss';
+import { background, container, h1, p } from '../WhoWeAre/WhoWeAre.module.scss';
 
 export const WhoWeAre = () => {
-  const { isDarkTheme } = useContext(ThemeContext);
   return (
     <section className={`${container}`}>
-      <div className={isDarkTheme ? backgroundDark : background}>
-        <h1 className={isDarkTheme ? h1Dark : h1}>Kim jesteśmy?</h1>
-        <p className={isDarkTheme ? pDark : p}>
+      <div className={`${background}`}>
+        <h1 className={`${h1}`}>Kim jesteśmy?</h1>
+        <p className={`${p}`}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida,
           nisl sit amet porta vulputate, odio tellus bibendum sapien, ac
           convallis massa lorem sed neque. Praesent sodales ex nisi, vel
