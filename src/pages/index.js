84a3c59b7833045b49275f250e0af8f1a23c1dd9
TheWoxPL <wojtek.bubula@onet.pl>
2024-01-04 14:25:58 +0100

    feature:  change index.js for test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { ItemTile } from '../components/ItemTile/ItemTile';
+import { SidebarMenu } from '../components/SidebarMenu/SidebarMenu';
 import { TitleText } from '../components/TitleText/TitleText';
 
-const IndexPage = () => (
-  <>
-    <TitleText />
-  </>
-);
+const IndexPage = () => {
+  const [visible, setVisible] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const toggle = () => {
+    setVisible((prev) => !prev);
+  };
+  const toggleTheme = () => {
+    setIsDarkTheme((prev) => !prev);
+  };
+
+  return (
+    <>
+      <TitleText />
+      <button onClick={toggle}>Menu</button>
+      <br></br>
+      <button onClick={toggleTheme}>ciemny</button>
+      <ItemTile />
+      <ItemTile />
+      <ItemTile />
+      <ItemTile />
+      <ItemTile />
+      <ItemTile />
+      <ItemTile />
+      <ItemTile />
+      <SidebarMenu
+        isSidebarMenuVisible={visible}
+        setIsSidebarMenuVisible={setVisible}
+        isDarkTheme={isDarkTheme}
+      />
+    </>
+  );
+};
 
 export default IndexPage;
 
