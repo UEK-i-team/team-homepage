6b10a184a0f593c9c25dbfc1397e4d0119b3491f
TheWoxPL <wojtek.bubula@onet.pl>
2024-01-05 21:09:48 +0100

    fix: remove unnecessary files and add padding around caron

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { ItemTile } from '../components/ItemTile/ItemTile';
-import { SidebarMenu } from '../components/SidebarMenu/SidebarMenu';
 import { TitleText } from '../components/TitleText/TitleText';
 
-const IndexPage = () => {
-  const [visible, setVisible] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-  const toggle = () => {
-    setVisible((prev) => !prev);
-  };
-  const toggleTheme = () => {
-    setIsDarkTheme((prev) => !prev);
-  };
-
-  return (
-    <>
-      <TitleText />
-      <button onClick={toggle}>Menu</button>
-      <br></br>
-      <button onClick={toggleTheme}>ciemny</button>
-      <ItemTile />
-      <ItemTile />
-      <ItemTile />
-      <ItemTile />
-      <ItemTile />
-      <ItemTile />
-      <ItemTile />
-      <ItemTile />
-      <SidebarMenu
-        isSidebarMenuVisible={visible}
-        setIsSidebarMenuVisible={setVisible}
-        isDarkTheme={isDarkTheme}
-      />
-    </>
-  );
-};
+const IndexPage = () => (
+  <>
+    <TitleText />
+  </>
+);
 
 export default IndexPage;
 
