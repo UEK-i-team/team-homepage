ffe48f1f5705680d72738625633a07318a09db67
h3770k1k1 <amagdarass@gmail.com>
2024-01-06 20:07:35 +0100

    fix: revert itemTile changes

--- src/components/ItemTile/ItemTile.js
+++ src/components/ItemTile/ItemTile.js
@@ -3,26 +3,14 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import {
-  container,
-  content,
-  darkTheme,
-  image,
-  text,
-  title,
-} from './ItemTile.module.scss';
+import { container, content, image, text, title } from './ItemTile.module.scss';
 
 export const ItemTile = ({
   itemTitle,
   itemText,
-  itemImage, 
+  itemImage,
   /* 'fileName.png', image from src/assets/images/ */
-<<<<<<< HEAD
   itemImageAlt,
-=======
-  projectImageAlt,
-  isDarkTheme,
->>>>>>> f756f58 (feature: wip)
 }) => {
   const data = useStaticQuery(graphql`
     query {
@@ -53,13 +41,8 @@ export const ItemTile = ({
   const imageSrc = getImage(matchedImage);
 
   return (
-<<<<<<< HEAD
     <div className={container}>
       <GatsbyImage image={imageSrc} alt={itemImageAlt} className={image} />
-=======
-    <div className={`${container} ${isDarkTheme ? darkTheme : ''}`}>
-      <GatsbyImage image={imageSrc} alt={projectImageAlt} className={image} />
->>>>>>> f756f58 (feature: wip)
       <div className={content}>
         <div className={title}>{itemTitle}</div>
         <div className={text}>{itemText}</div>
@@ -69,16 +52,8 @@ export const ItemTile = ({
 };
 
 ItemTile.propTypes = {
-<<<<<<< HEAD
   itemTitle: PropTypes.string.isRequired,
   itemText: PropTypes.string.isRequired,
   itemImage: PropTypes.string.isRequired,
   itemImageAlt: PropTypes.string.isRequired,
-=======
-  projectTitle: PropTypes.string.isRequired,
-  projectText: PropTypes.string.isRequired,
-  projectImage: PropTypes.string.isRequired,
-  projectImageAlt: PropTypes.string.isRequired,
-  isDarkTheme: PropTypes.bool.isRequired,
->>>>>>> f756f58 (feature: wip)
 };
