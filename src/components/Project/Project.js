77c9b90354392001aab833d108a34e71b32c374d
Oliwier Matus <matusoliwier@gmail.com>
2024-01-07 21:53:27 +0100

    fix: changed class import and arrow

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,13 +3,15 @@ import React from 'react';
 
 import { mobileProject, mobileProjectTitle } from './Project.module.scss';
 
+const ARROW = <>&#10140;</>;
+
 export const Project = ({ link, description, title }) => {
   return (
     <>
-      <h1 className={`${mobileProjectTitle}`}>{title}</h1>
-      <div className={`${mobileProject}`}>
+      <h1 className={mobileProjectTitle}>{title}</h1>
+      <div className={mobileProject}>
         <div>{description}</div>
-        <a href={link}>Zobacz nasz projekt w praktyce {/* ARROW */} &#10140;</a>
+        <a href={link}>Zobacz nasz projekt w praktyce {ARROW}</a>
       </div>
     </>
   );
