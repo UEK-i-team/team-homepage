edde7da2e67fc593327564003521f96aceea8204
Szymon Brud <szymon.brud@crodu.com>
2024-01-09 20:06:09 +0100

    fix: delete yarn.lock, delete TitleText component

--- src/components/TitleText/TitleText.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-
-import { ThemeContext } from '../../context/ThemeContex';
-import { title, titleDark } from './TitleText.module.scss';
-
-export const TitleText = () => {
-  const { theme } = useContext(ThemeContext);
-  return <h1 className={theme(title, titleDark)}>Hello on team-homepage</h1>;
-};
