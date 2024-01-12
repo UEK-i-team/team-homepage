f62c7524f0f165bf971807536f2a1c7635f83860
Dawid <david.s17q@gmail.com>
2024-01-12 20:16:14 +0100

    fix: changed css  classes "%" scaling into "px" scaling

--- src/components/WhoWeAre/WhoWeAre.js
+++ src/components/WhoWeAre/WhoWeAre.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { ThemeContext } from '../../context/ThemeContext';
+import { ThemeContext } from '../../context/ThemeContex';
 import {
   background,
   backgroundDark,
