fd7f181fdcac61cff6a04e970fc2ce4fc1a3bb30
Szymon Brud <szymon.brud@crodu.com>
2024-01-24 14:01:40 +0100

    feature: test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import { Navbar } from '../components/Navbar/Navbar';
+import { WhoWeAre } from '../components/WhoWeAre/WhoWeAre';
 import { ThemeProvider } from '../context/ThemeContex';
 const IndexPage = () => (
   <>
     <ThemeProvider>
       <Navbar />
+      <WhoWeAre />
+      <h1>hello</h1>
     </ThemeProvider>
   </>
 );
