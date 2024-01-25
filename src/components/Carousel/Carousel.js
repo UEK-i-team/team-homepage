4a68110f71cf256b7513e57e736b6f6e2f5fcb9e
Jakub Otto <227225@student.uek.krakow.pl>
2024-01-25 18:59:16 +0100

    feature: create carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,73 @@
+import './Carousel.module.scss';
+
+import React from 'react';
+import { useState } from 'react';
+
+import ActiveDot from '../../assets/svgs/ActiveDot.svg';
+import Dot from '../../assets/svgs/Dot.svg';
+import Frame from '../../assets/svgs/Frame.svg';
+import LeftArrowSvg from '../../assets/svgs/LeftArrowSvg.svg';
+import RightArrowSvg from '../../assets/svgs/RightArrowSvg.svg';
+import {
+  arrowContainer,
+  carousel,
+  img,
+  indicator,
+  indicators,
+  inner,
+  leftArrow,
+  rightArrow,
+} from './Carousel.module.scss';
+import ph from './imgs/ph.jpg';
+import ph1 from './imgs/ph1.jpg';
+import ph2 from './imgs/ph2.jpg';
+import ph3 from './imgs/ph3.jpg';
+
+export const Carousel = () => {
+  const images = [ph, ph1, ph2, ph3];
+  const length = images.length;
+
+  const [index, setIndex] = useState(0);
+
+  const handlePrevious = () => {
+    const newIndex = index - 1;
+    setIndex(newIndex < 0 ? length - 1 : newIndex);
+  };
+
+  const handleNext = () => {
+    const newIndex = index + 1;
+    setIndex(newIndex >= length ? 0 : newIndex);
+  };
+
+  return (
+    <div className={`${carousel}`}>
+      <div className={`${inner}`}>
+        <img src={images[index]} className={`${img}`} />
+
+        <div
+          className={`${leftArrow} ${arrowContainer}`}
+          onClick={handlePrevious}
+        >
+          <LeftArrowSvg />
+        </div>
+
+        <div className={`${rightArrow} ${arrowContainer}`} onClick={handleNext}>
+          <RightArrowSvg />
+        </div>
+
+        <div className={`${indicators}`}>
+          {images.map((_, i) => (
+            <div
+              key={i}
+              className={`${indicator} ${i === index ? 'active' : ''}`}
+              onClick={() => setIndex(i)}
+            >
+              {i === index ? <ActiveDot /> : <Dot />}
+            </div>
+          ))}
+          <Frame />
+        </div>
+      </div>
+    </div>
+  );
+};
