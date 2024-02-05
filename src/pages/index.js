e8cd2063b8ad886cf4ec943049905756f3394a72
abc <115871378+dontmentio@users.noreply.github.com>
2024-02-05 23:59:44 +0100

    feature: create mobile main page view (#39)
    * fix: fix background below footer component
    
    * feature: create main page view
    
    * fix: fix WhoWeAre dark theme style
    
    * fix: styling for mobile view aligned with figma

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,15 @@ import React from 'react';
 
 import { Footer } from '../components/Footer/Footer';
 import { Navbar } from '../components/Navbar/Navbar';
+import { OurProjects } from '../components/OurProjects/OurProjects';
+import { WhoWeAre } from '../components/WhoWeAre/WhoWeAre';
 import { ThemeProvider } from '../context/ThemeContex';
 const IndexPage = () => (
   <>
     <ThemeProvider>
       <Navbar />
+      <WhoWeAre />
+      <OurProjects />
       <Footer />
     </ThemeProvider>
   </>
