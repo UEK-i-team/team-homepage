0d9ab73c92182b930800c599693c3b5c5dbafa4d
Magdalena <amagdarass@gmail.com>
2024-05-05 21:59:14 +0200

    fix: translation

--- src/i18n/enTranslation.js
+++ src/i18n/enTranslation.js
@@ -23,6 +23,9 @@ const enTranslations = {
     ariaLabelSidebarCloseButton: 'close sidebar button',
     ariaLabelLangButton: 'Language switching button',
     ariaLabelThemeButton: 'Theme switching button',
+    heading: 'Who we are?',
+    description:
+      'The First of Her Name, the Unburnt, Queen of Meereen, Queen of the Andals and the Rhoynar and the First Men, Khalisee of the Great Grass Sea, Breaker of Chains and Mother of Dragons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
   },
 };
 
