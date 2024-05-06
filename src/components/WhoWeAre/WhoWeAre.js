aaedefe7932b87974f73afb096b428d6095f82e8
Magdalena Raś <87210052+h3770k1k1@users.noreply.github.com>
2024-05-06 11:35:42 +0200

    Fix/translation (#68)
    * fix: change p element
    
    * fix: change p element
    
    * feature: carousel (#30)
    
    * feature: create carousel
    
    * fix: carousel position and fix indicators
    
    * fix: image dimensions
    
    * fix: move carousel into project component
    
    * fix: mobile view, animations, spam block
    
    * fix: automatic slide, finger swipe
    
    * fix: finger swipe delete
    
    * fix: simpler function, hooks, prevs
    
    * fix: props, vars, shorter timeout, css classes change
    
    * fix: fix html structure in project
    
    * fix: names change
    
    * fix: add components to index
    
    ---------
    
    Co-authored-by: Jakub <jakub@MacBook-Pro-Jakub.local>
    Co-authored-by: Jakub <jakub@mbp-jakub.home>
    
    * feature: created project-view subpage (#38)
    
    * feature: change language button (#36)
    
    * feature: change language button
    
    * fix: translation
    
    ---------
    
    Co-authored-by: Kamil <kamilciesla34@gmail.com>
    Co-authored-by: Jakub Otto <114911926+JakubOtto@users.noreply.github.com>
    Co-authored-by: Jakub <jakub@MacBook-Pro-Jakub.local>
    Co-authored-by: Jakub <jakub@mbp-jakub.home>
    Co-authored-by: Dawid Szmitka <146339535+hamburger231@users.noreply.github.com>
    Co-authored-by: Wojciech Bubula <92746206+TheWoxPL@users.noreply.github.com>

--- src/components/WhoWeAre/WhoWeAre.js
+++ src/components/WhoWeAre/WhoWeAre.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { ThemeContext } from '../../context/ThemeContex';
 import {
@@ -12,22 +13,14 @@ import {
 } from '../WhoWeAre/WhoWeAre.module.scss';
 
 export const WhoWeAre = () => {
+  const { t } = useTranslation();
   const { theme } = useContext(ThemeContext);
+
   return (
     <section className={`${container}`}>
       <div className={theme(backgroundLight, backgroundDark)}>
-        <h1 className={theme(h1Light, h1Dark)}>Kim jesteśmy?</h1>
-        <p className={theme(pLight, pDark)}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida,
-          nisl sit amet porta vulputate, odio tellus bibendum sapien, ac
-          convallis massa lorem sed neque. Praesent sodales ex nisi, vel
-          imperdiet massa varius id. Vestibulum nec ultrices nulla, eu euismod
-          nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-          Maecenas gravida lacus eu mattis tristique. Etiam porta fringilla
-          urna, nec condimentum sapien luctus eget. Etiam iaculis, turpis vitae
-          eleifend tincidunt, lacus nibh suscipit tortor, vitae molestie erat
-          erat at ex. Fusce nec purus ac lectus iaculis congue.
-        </p>
+        <h1 className={theme(h1Light, h1Dark)}>{t('heading')}</h1>
+        <p className={theme(pLight, pDark)}>{t('description')}</p>
       </div>
     </section>
   );
