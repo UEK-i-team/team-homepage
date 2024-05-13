f50d6a9bf4e7b9db7d50ff5dd8dae4510923d183
Piotr Grochala <64172157+grochalapiotr@users.noreply.github.com>
2024-05-13 15:34:52 +0200

    Fix/lang prop and aria label on sidebar (#70)
    * fix: adjusted font size in item tile on tablet view
    
    * fix: add lang prop to html and fix aria label on sidebar
    
    * Revert "fix: adjusted font size in item tile on tablet view"
    
    This reverts commit cbc12228326b5411e9281891a0b5525bf64e502b.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { OurProjects } from '../components/OurProjects/OurProjects';
 import { WhoWeAre } from '../components/WhoWeAre/WhoWeAre';
-const IndexPage = () => (
-  <>
-    <WhoWeAre />
-    <OurProjects />
-  </>
-);
+
+const IndexPage = () => {
+  const { i18n } = useTranslation();
+  const currLang = i18n.language;
+  useEffect(() => {
+    document.documentElement.lang = currLang; // Set your language code here
+  }, [currLang]);
+
+  return (
+    <>
+      <WhoWeAre />
+      <OurProjects />
+    </>
+  );
+};
 
 export default IndexPage;
 
