f7f4bf9067815bb9b518fe5839cdd208e8bb1b99
Magdalena <amagdarass@gmail.com>
2024-06-22 12:04:23 +0200

    fix: delete line

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,8 @@
-import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
+import React from 'react';
 
 import NotFound from '../components/NotFound/NotFound';
 
 const NotFoundPage = () => {
-  const { i18n } = useTranslation();
-  const currLang = i18n.language;
-  useEffect(() => {
-    document.documentElement.lang = currLang;
-  }, [currLang]);
-
   return (
     <>
       <NotFound />
