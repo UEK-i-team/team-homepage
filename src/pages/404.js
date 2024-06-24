c9162e5147ff7db9cede2061f335120ee976e21c
Magdalena <amagdarass@gmail.com>
2024-06-24 11:20:19 +0200

    fix: add if conditionals

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import NotFound from '../components/NotFound/NotFound';
 
 const NotFoundPage = () => {
+  const { i18n } = useTranslation();
+  const currLang = i18n.language;
+  useEffect(() => {
+    if (typeof document !== 'undefined' && document !== null) {
+      document.documentElement.lang = currLang;
+    }
+  }, [currLang]);
   return (
     <>
       <NotFound />
