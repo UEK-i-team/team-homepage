506979ee3fd1c4e47397961c4b1b6c258f38536b
Magdalena Raś <87210052+h3770k1k1@users.noreply.github.com>
2024-07-15 10:36:01 +0200

    fix: new robot animarion file (#82)
    * fix: new robot animarion file
    
    * feature: add isPaused

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import { Link } from 'gatsby';
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import Lottie from 'react-lottie';
 
-import animationDataDark from '../../assets/svgs/ErrorRobot.json';
-import animationData from '../../assets/svgs/ErrorRobotDark.json';
+import ErrorRobot from '../../assets/svgs/ErrorRobot.svg';
+import ErrorRobotDark from '../../assets/svgs/ErrorRobotDark.svg';
+import ErrorRobotDarkPaused from '../../assets/svgs/ErrorRobotDarkPaused.svg';
+import ErrorRobotPaused from '../../assets/svgs/ErrorRobotPaused.svg';
+import { useAnimationContext } from '../../context/AnimationsContext';
 import { ThemeContext } from '../../context/ThemeContex';
 import {
   animationContainer,
@@ -18,71 +20,33 @@ import {
 } from './NotFound.module.scss';
 
 const NotFound = () => {
-  const [isStopped] = useState(true);
   const { isDarkTheme } = useContext(ThemeContext);
   const { t } = useTranslation();
+  const { isPaused } = useAnimationContext();
 
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationDataDark,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
-  const defaultOptionsDark = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
+  let RobotSvg;
+  if (isDarkTheme) {
+    RobotSvg = isPaused ? ErrorRobotPaused : ErrorRobot;
+  } else {
+    RobotSvg = isPaused ? ErrorRobotDarkPaused : ErrorRobotDark;
+  }
 
   return (
-    <>
-      {isDarkTheme ? (
-        <div className={mainContainerDark}>
-          <div className={animationContainer}>
-            {' '}
-            <Lottie
-              options={defaultOptions}
-              width={128.56872}
-              height={161.77344}
-              isStopped={isStopped}
-            />
-          </div>{' '}
-          <div className={infoTextDark}>
-            <h1>404</h1>
-            <h2>{t('notFoundHeader')}</h2>
-            <h4>{t('notFoundDescription')}</h4>
-            <Link to="/" className={mainPageLink}>
-              <div className={goBackButtonDark}>{t('notFoundButton')}</div>
-            </Link>
+    <div className={isDarkTheme ? mainContainerDark : mainContainer}>
+      <div className={animationContainer}>
+        <RobotSvg width="107.14" height="137.81" />
+      </div>
+      <div className={isDarkTheme ? infoTextDark : infoText}>
+        <h1>404</h1>
+        <h2>{t('notFoundHeader')}</h2>
+        <h4>{t('notFoundDescription')}</h4>
+        <Link to="/" className={mainPageLink}>
+          <div className={isDarkTheme ? goBackButtonDark : goBackButton}>
+            {t('notFoundButton')}
           </div>
-        </div>
-      ) : (
-        <div className={mainContainer}>
-          {' '}
-          <div className={animationContainer}>
-            <Lottie
-              options={defaultOptionsDark}
-              width={128.56872}
-              height={161.77344}
-              isStopped={isStopped}
-            />
-          </div>{' '}
-          <div className={infoText}>
-            <h1>404</h1>
-            <h2>{t('notFoundHeader')}</h2>
-            <h4>{t('notFoundDescription')}</h4>
-            <Link to="/" className={mainPageLink}>
-              <div className={goBackButton}>{t('notFoundButton')}</div>
-            </Link>
-          </div>
-        </div>
-      )}
-    </>
+        </Link>
+      </div>
+    </div>
   );
 };
 
